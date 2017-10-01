eb1ac3e7fa4bdadd2af70d2faf8a944d542f5f11
Daniel Carlson <kouetchua.carlson@skylabase.com>
2017-10-01 06:01:33 +0100

    React (#14)
    * Added some reducers and dummy data file
    
    * Fixing changes too large issue
    
    * Improved reducers and got data

--- techstack/src/App.js
+++ techstack/src/App.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import { LibraryList } from './components/LibraryList';
 
 const App = () => {
     return ( 
-      <Provider store = { createStore(reducers) } >
-          <Header headerText = "Tech Stack" />
-      </Provider>
+	    <Provider store = { createStore(reducers) }>
+        	<View>
+        		<Header headerText = "Tech Stack" />
+        		<LibraryList />
+        	</View> 
+	    </Provider>
     )
-};
-
+}
 export default App;
