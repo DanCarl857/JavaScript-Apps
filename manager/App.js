6e1d07005f6879142a0456827648ee30ee79ef40
DanCarl857 <dancarl758@gmail.com>
2017-10-06 22:03:46 +0100

    Added AuthReducer

--- manager/App.js
+++ manager/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { View, Text } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { View } from 'react-native';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 
 import reducers from './src/reducers';
 import LoginForm from './src/components/LoginForm';
@@ -22,8 +23,10 @@ class App extends Component {
 
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View>
           <LoginForm />
         </View>
