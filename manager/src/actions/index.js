6d1b8e957803ce0e89427ce989d1d06ef1d09eb4
Daniel Carlson <kouetchua.carlson@skylabase.com>
2017-10-08 18:34:54 +0100

    Fixed issue with forms in manager app (#19)
    * initial commit for memegenerator app
    
    * Added tech stack app
    
    * Added some reducers and dummy data file
    
    * Fixing changes too large issue
    
    * Improved reducers and got data
    
    * Implemented ListView
    
    * Finished tech stack app
    
    * Added manager app with boilerplate code and more
    
    * Added AuthReducer
    
    * finished with Authentication reducers
    
    * Fixed issues with forms in manager

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -10,21 +10,21 @@ import {
 // Action Creators
 export const emailChanged = (text) => {
   return {
-    type: 'EMAIL_CHANGED',
+    type: EMAIL_CHANGED,
     payload: text
-  }
+  };
 };
 
 export const passwordChanged = (text) => {
   return {
-    type: 'PASSWORD_CHANGED',
+    type: PASSWORD_CHANGED,
     payload: text
-  }
+  };
 };
 
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
-    dispatch({ type: LOGIN_USER })
+    dispatch({ type: LOGIN_USER });
     
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
@@ -32,21 +32,19 @@ export const loginUser = ({ email, password }) => {
         console.log(error);
 
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then(user => loginUserSuccess(dispatch,user))
+          .then(user => loginUserSuccess(dispatch, user))
           .catch(() => loginUserFail(dispatch));
       });
   };
 };
 
 const loginUserFail = (dispatch) => {
-  dispatch({
-    type: LOGIN_USER_FAIL
-  })
-}
+  dispatch({ type: LOGIN_USER_FAIL });
+};
 
 const loginUserSuccess = (dispatch, user) => {
   dispatch({
     type: LOGIN_USER_SUCCESS,
     payload: user
-  })
-}
+  });
+};
--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -5,7 +5,6 @@ import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
-
   onEmailChange(text) {
 
     console.log(text);
@@ -19,7 +18,19 @@ class LoginForm extends Component {
   onButtonPress() {
     const { email, password } = this.props;
 
-    this.props.loginUser({email, password});
+    this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
   }
 
   renderButton() {
@@ -42,7 +53,7 @@ class LoginForm extends Component {
             label="Email"
             placeholder="email@gmail.com"
             onChangeText={this.onEmailChange.bind(this)}
-            value={() => this.props.email.trim()}
+            value={this.props.email}
           />
         </CardSection>
 
@@ -52,7 +63,7 @@ class LoginForm extends Component {
             label="Password"
             placeholder="password"
             onChangeText={this.onPasswordChange.bind(this)}
-            value={() => this.props.password.trim()}
+            value={this.props.password}
           />
         </CardSection>
 
@@ -64,7 +75,15 @@ class LoginForm extends Component {
           {this.renderButton()}
         </CardSection>
       </Card>
-    )
+    );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 };
 
@@ -80,7 +99,7 @@ const mapStateToProps = ({ auth }) => {
   const { email, password, error, loading } = auth;
 
   return { email, password, error, loading };
-}
+};
 
 export default connect(mapStateToProps, {
   emailChanged, passwordChanged, loginUser
